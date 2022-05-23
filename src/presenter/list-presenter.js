61be6c9566c394b9e21356fa2030f42bc0d669eb
Mikhail-Sakharov <sakharov_mikhail@hotmail.com>
2022-05-24 01:58:06 +0300

    Переименовал основной презентер

---
+++ src/presenter/list-presenter.js
@@ -0,0 +1,77 @@
+import {render, RenderPosition} from '../framework/render.js';
+import SortView from '../view/sort-view.js';
+import ListView from '../view/list-view.js';
+import EmptyListView from '../view/empty-list-msg.js';
+import FiltersView from '../view/filters-view.js';
+import {OFFERS} from '../mock/offers.js';
+import {DESTINATIONS} from '../mock/destinations.js';
+import PointPresenter from './point-presenter.js';
+
+const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
+
+export default class ListPresenter {
+  #listComponent = new ListView();
+
+  #pointPresenter = new Map();
+
+  #listPoints = [];
+
+  #handlePointChange = (updatedPoint) => {
+    this.#listPoints = updateItem(this.#listPoints, updatedPoint);
+    this.#pointPresenter.get(updatedPoint.id).init(updatedPoint);
+  };
+
+  #renderEmptyList = () => {
+    const filters = new FiltersView();
+    const checkedFilter = filters.element.querySelector('input[name="trip-filter"]:checked');
+    const isChecked = !!(checkedFilter);
+    const filterValue = isChecked ? checkedFilter.value : 'everything';
+    render(new EmptyListView(filterValue), this.listContainer, RenderPosition.AFTERBEGIN);
+  };
+
+  #renderList = () => {
+    render(this.#listComponent, this.listContainer, RenderPosition.AFTERBEGIN);
+  };
+
+  #renderSort = (points) => {
+    render(new SortView(points), this.#listComponent.element, RenderPosition.AFTERBEGIN);
+  };
+
+  #renderPoints = (points) => {
+    points.forEach((point) => {
+      const specifiedTypeOffers = OFFERS.find((offer) => offer.type === point.type).offers;
+      const pointPresenter = new PointPresenter(this.#listComponent.element, this.#handlePointChange);
+      pointPresenter.init(point, specifiedTypeOffers, DESTINATIONS);
+      this.#pointPresenter.set(point.id, pointPresenter);
+    });
+  };
+
+  #clearPointList = () => {
+    this.#pointPresenter.forEach((presenter) => presenter.destroy());
+    this.#pointPresenter.clear();
+  };
+
+  init = (listContainer, points) => {
+    this.listContainer = listContainer;
+
+    if (!points || points.length === 0) {
+      this.#renderEmptyList();
+    } else {
+      this.#renderList();
+      this.#renderSort(points);
+      this.#renderPoints(points);
+    }
+  };
+}
