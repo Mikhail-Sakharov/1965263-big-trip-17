1af763de0dccf70f4c2b7655bae1c5cc07033ea4
Mikhail-Sakharov <sakharov_mikhail@hotmail.com>
2022-05-25 10:44:53 +0300

    Реализовал логику сортировки точек маршрута

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,8 +1,14 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
+export const SortType = {
+  DEFAULT: 'default',
+  TIME_DOWN: 'time-down',
+  PRICE_DOWN: 'price-down',
+};
+
 const createSortTemplate = () => `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
             <div class="trip-sort__item  trip-sort__item--day">
-              <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
+              <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" data-sort-type="${SortType.DEFAULT}">
               <label class="trip-sort__btn" for="sort-day">Day</label>
             </div>
 
@@ -12,12 +18,12 @@ const createSortTemplate = () => `<form class="trip-events__trip-sort  trip-sort
             </div>
 
             <div class="trip-sort__item  trip-sort__item--time">
-              <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+              <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" data-sort-type="${SortType.TIME_DOWN}">
               <label class="trip-sort__btn" for="sort-time">Time</label>
             </div>
 
             <div class="trip-sort__item  trip-sort__item--price">
-              <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
+              <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" data-sort-type="${SortType.PRICE_DOWN}">
               <label class="trip-sort__btn" for="sort-price">Price</label>
             </div>
 
@@ -31,4 +37,18 @@ export default class SortView extends AbstractView {
   get template() {
     return createSortTemplate();
   }
+
+  setSortTypeChangeHandler = (callback) => {
+    this._callback.sortTypeChange = callback;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  };
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'INPUT') {
+      return;
+    }
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  };
 }
