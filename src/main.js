ae3a69f61018bfc516c2b57966c41165959ae044
Mikhail-Sakharov <sakharov_mikhail@hotmail.com>
2022-06-08 20:41:15 +0300

    Добавил презентер заголовка и подписку на изменение данных

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import {render, RenderPosition} from './framework/render.js';
-import TitleView from './view/title-view.js';
+//import {render, RenderPosition} from './framework/render.js';
+//import TitleView from './view/title-view.js';
+import TitlePresenter from './presenter/title-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import ListPresenter from './presenter/list-presenter.js';
 import PointModel from './model/point-model.js';
 import FilterModel from './model/filter-model.js';
-import {returnTitleDuration, calculatePrice} from './util.js';
+//import {returnTitleDuration, calculatePrice} from './util.js';
 import PointsApiService from './api-services/points-api-service.js';
 
 const AUTHORIZATION = 'Basic z1f7ti3dx123f7t3SMG888';
@@ -18,6 +19,7 @@ const tripEventsContainerElement = document.querySelector('.trip-events');
 const filterModel = new FilterModel();
 const pointModel = new PointModel(new PointsApiService(END_POINT, AUTHORIZATION));
 
+const titlePresenter = new TitlePresenter(titleContainerElement, pointModel);
 const filterPresenter = new FilterPresenter(filtersContainerElement, filterModel, pointModel);
 const listPresenter = new ListPresenter(tripEventsContainerElement, pointModel, filterModel);
 
@@ -35,18 +37,6 @@ pointModel.init().finally(() => {
   newPointButtonElement.addEventListener('click', newPointButtonClickHandler);
 });
 
-const points = pointModel.points;
-const offers = pointModel.offers;
-//console.log(points, offers);
-
-const destinationNames = points.slice().sort((nextItem, currentItem) => new Date(nextItem.dateFrom) - new Date(currentItem.dateFrom)).map((point) => point.destination.name);
-
-if (points && points.length !== 0) {
-  const totalPrice = calculatePrice(points, offers);
-  const titleDuration = returnTitleDuration(points);
-
-  render(new TitleView(destinationNames, totalPrice, titleDuration), titleContainerElement, RenderPosition.AFTERBEGIN);
-}
-
+titlePresenter.init();
 filterPresenter.init();
 listPresenter.init();
